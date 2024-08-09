b1ab5593383dad4283d59c8724a2b468206372bf
Felgi <felipeferraridoval@gmail.com>
2024-08-09 08:16:23 -0300

    lo logré bro, lo logré

--- Código/Front/Frames Lista Objetos/Creacion-publicacion.js
+++ Código/Front/Frames Lista Objetos/Creacion-publicacion.js
@@ -3,7 +3,7 @@
 function comentar(){
     let lista = document.createElement("div");
     lista.textContent = `${document.getElementById("input").value}`;
-    document.getElementById("comenta").appendChild(lista);
+    document.getElementById("coment-box").appendChild(lista);
     console.log("hola");
 }
 document.getElementById("boti").addEventListener("click", comentar);

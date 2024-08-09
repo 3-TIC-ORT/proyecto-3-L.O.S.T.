3329da7e0c40e35666463cb5da820988f60386cf
Facuindo <santinoruani@gmail.com>
2024-08-09 11:54:29 -0300

    SII

--- Experimentos/index.js
+++ Experimentos/index.js
@@ -1,7 +1,6 @@
 const { readFileSync } = require('fs');
 const { parse } = require('csv-parse/sync');
-const { CsvStringifier } = require('csv-writer/src/lib/csv-stringifiers/abstract');
 
 let contenido = readFileSync('Video Games Sales.csv', 'utf-8');
-let interpretado = parse(contenido);
+let interpretado = parse(contenido, {columns: true});
 console.log(interpretado);

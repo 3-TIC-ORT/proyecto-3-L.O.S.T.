1f351bfec7242dc3544ce9e86485dd9dea2657d9
Felgi <felipeferraridoval@gmail.com>
2024-08-09 10:41:01 -0300

    Hice algunos comentarios en el código, lo cual deberíamos hacer más

--- Código/Front/Frames Lista Objetos/Creacion-publicacion.js
+++ Código/Front/Frames Lista Objetos/Creacion-publicacion.js
@@ -2,12 +2,14 @@
 let userNum = 0
 function comentar(){
 
-
+    //Hago que haya un h4 que tenga de nombre un número de usuario único y que se muestre arriba del comentario de quien lo creo, además de que no va a haber un user 0
 
     userNum++
     let user = document.createElement("h4");
     user.textContent = `Usuario ${userNum}:`
     document.getElementById("coment-box").appendChild(user);
+
+    //Creo el comentario, lo guarda y los displayea
 
     let coment = document.createElement("div"); 
     coment.textContent = `${document.getElementById("InputComentario").value}`;

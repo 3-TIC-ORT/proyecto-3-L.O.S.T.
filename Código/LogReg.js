634154bbefbe39f45850786178cb81a01237a2f0
Facuindo <santinoruani@gmail.com>
2024-08-23 10:44:37 -0300

    Ash

--- Código/LogReg.js
+++ Código/LogReg.js
@@ -1,23 +1,65 @@
 import readlineSync from "readline-sync";
-import fs, { writeFileSync } from 'fs'
+import fs from 'fs'
 
 let usuarios = JSON.parse(fs.readFileSync("users.json"))
-function registro(){
+function registroConsola(){
     let usuario = {};
-    while(usuario.user === undefined || usuario.user === null || usuario.user.length > 32){
+    let usuarioExistente = false;
+    while(usuario.user === undefined || usuario.user === null || usuario.user.length > 32 || usuarioExistente){
+        usuarioExistente = false
         usuario.user = readlineSync.question("Ingrese usuario: ");
         if(usuario.user.length > 32){
             console.log("Su usuario no puede tener más de 32 caracteres")
         }
+        for(let i = 0; i < usuarios.length; i++){
+            if(usuario.user == usuarios[i].user){
+                console.log("Nombre de usuario en uso")
+                usuarioExistente = true;
+            }
+        }
     }
-    while(usuario.password === undefined || usuario.password === null || usuario.password.length > 32 || usuario.password.length < 8){
+    while(usuario.password === undefined || usuario.password === null || usuario.password.length > 32 || usuario.password.length < 8 || usuario.password.match(/[a-z]/) == null || usuario.password.match(/[A-Z]/) == null || usuario.password.match(/[0-9]/) == null){
         usuario.password = readlineSync.question("Ingrese contraseña: ");
-        if(usuario.password.length > 32 || usuario.password.length < 8){
-            console.log("La contraseña debe tener entre 8 y 32 caracteres")
+        if(usuario.password.length > 32 || usuario.password.length < 8 || usuario.password.match(/[a-z]/) == null || usuario.password.match(/[A-Z]/) == null || usuario.password.match(/[0-9]/) == null){
+            console.log("La contraseña debe tener entre 8 y 32 caracteres, al menos una minúscul, una mayúscula y un número")
         }
     }
     usuario.id = usuarios.length;
     usuarios.push(usuario);
-    writeFileSync("users.json", JSON.stringify(usuarios,null, 2));
+    fs.writeFileSync("users.json", JSON.stringify(usuarios,null, 2));
 }
-registro();
+
+function registroJson(){
+    let usuario = {};
+    let input = JSON.parse(fs.readFileSync('Input.Json'))
+    let usuarioCon = true;
+    usuario.user = input.user;
+    if(usuario.user.length > 32){
+        console.log("Su usuario no puede tener más de 32 caracteres")
+        usuarioCon = false;
+    }
+    for(let i = 0; i < usuarios.length; i++){
+        if(usuario.user == usuarios[i].user){
+            console.log("Nombre de usuario en uso")
+            usuarioCon = false;
+        }
+    }
+    usuario.password = input.password;
+    if(usuario.password.length > 32 || usuario.password.length < 8 || usuario.password.match(/[a-z]/) == null && usuario.password.includes("ñ") === false || usuario.password.match(/[A-Z]/) == null && usuario.password.includes("Ñ") === false || usuario.password.match(/[0-9]/) == null){
+        console.log("La contraseña debe tener entre 8 y 32 caracteres, al menos una minúscul, una mayúscula y un número")
+        usuarioCon = false;
+    }
+    if(usuarioCon){
+        usuario.id = usuarios.length;
+        usuarios.push(usuario);
+        fs.writeFileSync("users.json", JSON.stringify(usuarios,null, 2));
+    }
+    else{
+        console.log("Hubo un error, así que no se guardó en la base de Datos")
+    }
+    
+}
+registroJson();
+// LoginJson(){
+
+// }

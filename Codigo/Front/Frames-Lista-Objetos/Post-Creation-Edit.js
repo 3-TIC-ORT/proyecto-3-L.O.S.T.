ed48e6afa67e1d0e112dbda46f56c4df41ae3a4d
Felgi <felipeferraridoval@gmail.com>
2024-09-10 14:44:33 -0300

    Funciona el guardado de input localmente, pero no se guarda en el json del back.

--- Codigo/Front/Frames-Lista-Objetos/Post-Creation-Edit.js
+++ Codigo/Front/Frames-Lista-Objetos/Post-Creation-Edit.js
@@ -6,23 +6,21 @@ let publicacion = {}
 //La siguiente funcion lo que hace es guardar toda la info de los inputs y enviarla al back.
 
 const form = document.querySelector(`form`)
-
+localStorage.setItem("userId", 0)
 form.addEventListener(`submit`, (e) => {
     e.preventDefault();
     formulario = e.target
-    publicacion.imagen = formulario.img;
-    publicacion.categoria = formulario.category;
-    publicacion.titulo = formulario.title;
-    publicacion.ubicacion = formulario.location;
-    publicacion.descripcion = formulario.description;
-    publicacion.dejado = formulario.placeLeft
-    publicacion.hora = formulario.time
-    let userName = localStorage.getItem(`userName`)
-    let userPassword = localStorage.getItem(`userPassword`)
-    let usuario = [JSON.parse(userPassword), JSON.parse(userName)]
+    publicacion.imagen = formulario.img.value;
+    publicacion.categoria = formulario.category.value;
+    publicacion.titulo = formulario.title.value;
+    publicacion.ubicacion = formulario.location.value;
+    publicacion.descripcion = formulario.description.value;
+    publicacion.dejado = formulario.placeLeft.value;
+    publicacion.hora = formulario.time.value;
+    let usuario = JSON.parse(localStorage.getItem("userId"))
     publicacion.creador = usuario;
     console.log(publicacion);
     // window.location.href = "ListaObjs.html"
-    postData(`crearPublicacion`, publicacion)
+    postData("crearPublicacion", publicacion)
 })
 

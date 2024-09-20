495ae20a6fbf69a86e99e9c8dc9d30afedd22a63
Santinorua <santinoruani@gmail.com>
2024-09-20 10:41:03 -0300

    Ahora se puede hacer unit testing

--- Codigo/Back/index.js
+++ Codigo/Back/index.js
@@ -137,6 +137,13 @@ onEvent("comentar", comentar);
 onEvent("mostrarNotificaciones", mostrarNotificaciones);
 
 
-if(require.main === module){
-    startServer();
+import * as url from 'node:url';
+
+if(import.meta.url.startsWith('file:')){
+    const modulePath = url.fileURLToPath(import.meta.url);
+    if(process.argv[1] === modulePath){
+        startServer();
+    }
 }
+
+

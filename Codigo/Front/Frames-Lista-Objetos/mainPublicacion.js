dcc48d931b231dd143ce6f07ae2567ca260f5d6f
Felgi <felipeferraridoval@gmail.com>
2024-09-22 22:18:29 -0300

    fix: Para comentar si o si necesitas estar logueado en una cuenta

--- Codigo/Front/Frames-Lista-Objetos/mainPublicacion.js
+++ Codigo/Front/Frames-Lista-Objetos/mainPublicacion.js
@@ -10,19 +10,23 @@ let publicaciones = localStorage.getItem("publicaciones");
 // DataLoader();
 
 //Creo el comentario, lo guarda, lo displayea y despues le manda el comentario al back.
-function Comentar(){
-    let user = document.createElement("h4");
-    user.textContent = JSON.parse(localStorage.getItem("userName"));
-    let coment = document.createElement("p"); 
-    coment.textContent = `${document.getElementById("InputComentario").value}`;
-    let container = document.createElement("article");
-    document.getElementById("coment-box").appendChild(container);
-    container.appendChild(user)
-    container.appendChild(coment)
-    comentario.comm = document.getElementById("InputComentario").value
-    comentario.id = localStorage.getItem("userId");
-    comentario.user = localStorage.getItem("userName");
-    postData("comentar", comentario);
+function Comentar(){z
+    if(localStorage.getItem("userId") === null) {
+        alert("Para hacer un comentario necesita haber iniciado sesión anteriormente")
+    } else {
+        let user = document.createElement("h4");
+        user.textContent = JSON.parse(localStorage.getItem("userName"));
+        let coment = document.createElement("p"); 
+        coment.textContent = `${document.getElementById("InputComentario").value}`;
+        let container = document.createElement("article");
+        document.getElementById("coment-box").appendChild(container);
+        container.appendChild(user)
+        container.appendChild(coment)
+        comentario.comm = document.getElementById("InputComentario").value
+        comentario.id = localStorage.getItem("userId");
+        comentario.user = localStorage.getItem("userName");
+        postData("comentar", comentario);
+    }
 } document.getElementById("enviar").addEventListener("click", Comentar);
 
 
